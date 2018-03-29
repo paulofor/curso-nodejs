6d5b32be255ca9e25f58aa8c5646541291f6ed6b
Paulo <paulofore@gmail.com>
2018-03-29 20:39:46 -0300

    Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -3,52 +3,14 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routers/dishRouter');
 const hostname = 'localhost';
 const port = 3232;
 
 const app = express();  
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-
-
-
-app.all('/dishes',(req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-});
-app.get('/dishes', (req,res,next) => {
-    res.end('Em breve pratos serão lançados...');
-});
-app.post('/dishes', (req,res,next) => {
-    res.end('Vou adicionar o prato: ' + req.body.name + 
-        ' com os detalhes: ' + req.body.description);
-});
-app.put('/dishes', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('PUT não suportado ');
-});
-app.delete('/dishes', (req,res,next) => {
-    res.end('Em breve pratos serão apagados...');
-});
-
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Em breve pratos serão lançados para: ' + req.params.dishId);
-});
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('POST não suportado para um id (' + req.params.dishId + ')');
-});
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Vai atualizar id: ' + req.params.dishId);
-    res.end('Vou alterar o prato: ' + req.body.name + 
-    ' com os detalhes: ' + req.body.description);
-});
-app.delete('/dishes/:dishId', (req,res,next) => {
-    res.end('Em breve prato será apagado id: ' + req.params.dishId);
-});
-
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
